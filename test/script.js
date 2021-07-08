9482a696fcd21d4b4d195e14a21ecae688e4de2f
Rajesh kumar <rajeshkumar@mallow-tech.com>
2021-07-08 16:39:11 +0530

    Updated gitignore and added test files

--- test/script.js
+++ test/script.js
@@ -1,2 +1,2 @@
-const sumOfAray =  require('sum-of-array');
-console.log(sumOfAray([1,2,3,4,5,6]))
+const sumOfAray =  require('@rajesh_kumar_kgm/sum-of-array');
+console.log(sumOfAray([1,2,3,4,5,6,7,8]))
